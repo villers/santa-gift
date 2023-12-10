0c6ca6451f7c5667d92ee327da22558f27f1a357
Mickaël VILLERS <villers.mickael.ext@homeserve.fr>
2023-12-10 16:41:55 +0100

    fix: assets links

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const nextConfig = {
     output: "export",
     basePath: "/santa-gift",
     assetPrefix: '/santa-gift',
-    images: {unoptimized: true},
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'villers.github.io',
+                port: '',
+                pathname: '/santa-gift/**',
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
