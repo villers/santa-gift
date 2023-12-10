374231a798e653e1238ea5acdc87f775130ce8fb
villers <mickael.villers@epitech.eu>
2023-12-10 15:07:46 +0000

    Deploying to gh-pages from @ villers/santa-gift@7614db0a4fce93eeb1e028d46d68b27650907369 🚀

---
+++ _next/static/chunks/app/page-6c5ccaf9fb2574d7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{6829:function(n,e,u){Promise.resolve().then(u.t.bind(u,4965,23))}},function(n){n.O(0,[965,971,938,744],function(){return n(n.s=6829)}),_N_E=n.O()}]);
